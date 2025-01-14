06ca8b7922539cc5c93379fbedd163b4ff5bb3d2
Yohai Reshef <yohai.reshef@gmail.com>
2025-01-14 14:32:59 +0200

    Added concrete implementation of ImageCacheService and added service to DI

--- MovieExplorer/Services/Interactors/ImageCachingService.swift
+++ MovieExplorer/Services/Interactors/ImageCachingService.swift
@@ -7,10 +7,36 @@
 
 import UIKit
 
-protocol ImageCachingServicing {
-    
+protocol ImageCacheServicing: CacheServicable {
+    func value(forKey key: Int) -> UIImage?
+    func save(_ value: UIImage, forKey key: Int)
+    func removeValue(forKey key: Int)
+    func clearCache()
 }
 
-final class ImageCachingService: ImageCachingServicing {
+final class ImageCacheService<CacheType: CacheServicable>: ImageCacheServicing
+    where CacheType.K == Int, CacheType.T == UIImage {
+
+    private let cacheService: CacheType
     
+    init(cacheService: CacheType) {
+        self.cacheService = cacheService
+    }
+    
+    func value(forKey key: Int) -> UIImage? {
+        return cacheService.value(forKey: key)
+    }
+    
+    func save(_ value: UIImage, forKey key: Int) {
+        cacheService.save(value, forKey: key)
+    }
+    
+    func removeValue(forKey key: Int) {
+        cacheService.removeValue(forKey: key)
+    }
+    
+    func clearCache() {
+        cacheService.clearCache()
+    }
 }
+
--- MovieExplorer/System/DependencyContainer.swift
+++ MovieExplorer/System/DependencyContainer.swift
@@ -5,25 +5,28 @@
 //  Created by Yohai on 06/01/2025.
 //
 
-import Foundation
+import UIKit
 
 protocol DependencyContaining {
     
     var networkService: NetworkServicing { get }
     var movieService: MovieServicing { get }
     var imageService: ImageServicing { get }
+    var imageCacheService: any ImageCacheServicing { get }
     
     func initializeServices()
     func provideMovieService() -> MovieServicing
     func provideImageService() -> ImageServicing
 }
 
-final class DependencyContainer: DependencyContaining {
+final class DependencyContainer: DependencyContaining {    
     
     // MARK: Contained services
     internal let networkService: NetworkServicing = NetworkService()
     internal lazy var movieService: MovieServicing = MovieService(networkService: networkService)
     internal lazy var imageService: ImageServicing = ImageService(networkService: networkService)
+    internal let imageCacheService: any ImageCacheServicing = ImageCacheService(cacheService: CacheService<Int, UIImage>())
+
     
     init() {
         initializeServices()
